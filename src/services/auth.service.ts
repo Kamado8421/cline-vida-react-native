f9f1d0244a67a7e72bbc69231836e563d2d79645
Kamado8421 <lucianomendes8421@gmail.com>
2025-04-04 22:53:44 -0300

    auth info

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { UserAuthProps } from "./models/user-auth.model";
 import { LoginPromisseType } from "./models/login-promisse.model";
-import { setStorageAccessToken } from "./storages.service";
+import { getStorageAccessToken, setStorageAccessToken } from "./storages.service";
 
+type UserApiType = {
+    info: {
+        phoneNumber?: string | undefined;
+        dateBirth?: Date | undefined;
+        cpf?: string | undefined;
+    };
+    user: {
+        id: string;
+        email: string;
+        isFullRegister: boolean;
+    };
+}
 export class AuthService {
-    constructor(private API_HOST: string) { }
+    constructor(private API_HOST: string = `${process.env.HOST_SERVER}`) { }
 
     async login(user: UserAuthProps): Promise<LoginPromisseType> {
         try {
@@ -44,6 +56,39 @@ export class AuthService {
                 message: 'Internal error',
                 success: false
             };
+        }
+    }
+
+
+    async getUser(): Promise<UserApiType | undefined | { message: string, success: boolean }> {
+        try {
+            const token = await getStorageAccessToken();
+
+            if (token) {
+                const url = `${this.API_HOST}/user`
+                const response = await axios.get(url, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+
+                if (response.data) {
+                    return {
+                        success: true,
+                        ...response.data
+                    }
+                }
+
+                return undefined;
+
+            }
+        } catch (error: any) {
+            if (error.response?.data?.error === 'Unauthorized') {
+                return {
+                    message: 'Token inválido ou usuário não encontrado.',
+                    success: false
+                };
+            }
         }
     }
 }
